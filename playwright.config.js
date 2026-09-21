1c32a8029df76fe7c440e6766c6f94ef8a06865d
imabhinavojha <abhinavojha59@hotmail.com>
2026-09-21 12:59:48 +0530

    Fix Playwright CI configuration: run headless in CI, headed locally

--- playwright.config.js
+++ playwright.config.js
@@ -32,7 +32,8 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    headless: false,
+    /* Run headless in CI, headed locally if preferred */
+    headless: !!process.env.CI,
     trace: 'retain-on-failure',
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
