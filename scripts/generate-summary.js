3682e6362e0c5b443ddb7417ab856a151042b756
imabhinavojha <abhinavojha59@hotmail.com>
2026-09-23 10:28:40 +0530

    change in folder structure.

--- scripts/generate-summary.js
+++ scripts/generate-summary.js
@@ -119,7 +119,7 @@ function processReport(report) {
         cutout: '65%'
       }
     };
-    md += `![Test Outcome Distribution](https://quickchart.io/chart?c=${encodeURIComponent(JSON.stringify(chartConfig))}&width=160&height=250)\n\n`;
+    md += `![Test Outcome Distribution](https://quickchart.io/chart?c=${encodeURIComponent(JSON.stringify(chartConfig))}&backgroundColor=white&width=260&height=150&devicePixelRatio=2)\n\n`;
   }
 
   // 2. Failed Tests (Open by default)
